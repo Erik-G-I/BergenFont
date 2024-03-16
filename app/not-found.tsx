fd296422de1506b50782417c49a89b2dc93a9719
Erik <erik.g.ingebrigtsen@gmail.com>
2024-03-16 19:08:29 +0100

    komma

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div className="blocktext">
       <h1 className="title">404: Kunne ikke finne siden</h1>
-      <p>Vi klarte ikke å finne det du lette etter dessverre :-(</p>
+      <p>Vi klarte ikke å finne det du lette etter, dessverre :-(</p>
     </div>
   );
 }
