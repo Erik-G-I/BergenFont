33ce1fd2ab8d633174fd06f4e3065ea55ebfb7d5
Erik <erik.g.ingebrigtsen@gmail.com>
2024-05-28 10:18:02 +0200

    lagt til funksjon for tilfeldig font på tittelen på hjemmesiden

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,52 @@
 import Link from "next/link";
 import loadFontList from "./readJSON";
 
+// var titleFont = (function RandomTitleFont() {
+//   var titleFonts = [
+//     "Coolvetica",
+//     "Computer Modern",
+//     "Aber Mono",
+//     "Great Warrior",
+//     "Retro Boldy",
+//     "Melno",
+//     "Rafaella",
+//     "Planet Kosmos",
+//     "Ringold-Clarendon",
+//     "Expressway",
+//     "Tuppence",
+//   ];
+//   var randomFont = Math.floor(Math.random() * titleFonts.length);
+//   return titleFonts[randomFont];
+// })();
+
 export default function Home() {
   return (
     <>
       <div className="pageblock" spellCheck="false">
-        <h1 className="title">Bergen Font</h1>
+        <h1
+          className="title"
+          style={{
+            fontFamily: (function RandomTitleFont() {
+              var titleFonts = [
+                "Coolvetica",
+                "Computer Modern",
+                "Aber Mono",
+                "Great Warrior",
+                "Retro Boldy",
+                "Melno",
+                "Rafaella",
+                "Planet Kosmos",
+                "Ringold-Clarendon",
+                "Expressway",
+                "Tuppence",
+              ];
+              var randomFont = Math.floor(Math.random() * titleFonts.length);
+              return titleFonts[randomFont];
+            })(),
+          }}
+        >
+          Bergen Font
+        </h1>
         <h3>Nylige funn:</h3>
         <div className="homeList">
           {loadFontList("/app/data/homeFonts.json")}
