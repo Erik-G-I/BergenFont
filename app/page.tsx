9af7e827db1bd55a648c15e8c68afbc511f57eea
Erik <erik.g.ingebrigtsen@gmail.com>
2024-09-05 21:42:03 +0200

    de-bratified

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
 import loadFontList from "./readJSON";
 
-var titleFont = "Coolvetica";
-
 export default function Home() {
   return (
     <>
       <div className="pageblock" spellCheck="false">
-        <h1 className="title">bergen font</h1>
+        <h1 className="title">Bergen Font</h1>
         <h3>Nylige funn:</h3>
         <div className="homeList">
           {loadFontList("/app/data/homeFonts.json")}
