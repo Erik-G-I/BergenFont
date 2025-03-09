21d735eeb321cebac3defaf3b6adff6bd7a2125b
Erik <erik.g.ingebrigtsen@gmail.com>
2025-03-09 03:59:42 +0100

    total refactor to astro

--- app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="pageblock">
-      <h1 className="title">404: Kunne ikke finne siden</h1>
-      <p>Vi klarte ikke å finne det du lette etter, dessverre :-(</p>
-    </div>
-  );
-}
