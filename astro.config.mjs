558790da045291c60b193404d657a490af941b7d
Erik <erik.g.ingebrigtsen@gmail.com>
2025-03-10 15:26:02 +0100

    added contact page. changes to about page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue()]
+});
