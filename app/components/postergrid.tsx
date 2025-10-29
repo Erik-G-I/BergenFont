06b065a38e417339710820def809dba93fb73964
Erik <erik.g.ingebrigtsen@gmail.com>
2025-10-29 14:56:44 +0100

    added social links to posters; needs work for large number of posters

--- app/components/postergrid.tsx
+++ app/components/postergrid.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image"
+import Link from "next/link";
 
 interface PosterGridProps{
     images: string[];
-    fonts: string[];
+    fonts?: string[];
+    socials?: string[];
     columns?: number;
     gap?: string;
 }
 
-export default function PosterGrid({images, fonts, columns=3, gap="1rem"}:PosterGridProps) {
+export default function PosterGrid({images, fonts=[], socials=[], columns=3, gap="1rem"}:PosterGridProps) {
     return(
         <div className="poster-grid"
         style={{
@@ -16,14 +18,16 @@ export default function PosterGrid({images, fonts, columns=3, gap="1rem"}:Poster
         }}>
             {images.map((src, i) => (
                 <div key={i} className="grid-item" style={{breakInside: "avoid", marginBottom: gap}}>
-                    <Image
+                    <Link href={`https://${socials[i]}`}>
+                        <Image
                         src={src}
                         alt={`image-${i}`}
                         width={400}
                         height={400}
                         style={{width:"100%", height:"auto", borderRadius:"8px"}}
                         loading="lazy"
-                    />
+                        />
+                    </Link>
                     <a href={`/fonts/${fonts[i]}`} download>
                         Last ned font
                     </a>
--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,26 @@ import loadFontList from "./readJSON";
 import PosterGrid from "./components/postergrid";
 
 export default function Home() {
-  const images= [
+  const images:string[] = [
     "/images/testC.png",
     "/images/testB.png",
     "/images/testA.png",
   ]
-  const fonts =[
-    "cmunti.ttf",
-    "Comic sans",
-    "Arial",
+  const fonts:string[] = [
+    "cmunrm.ttf",
+    "cmunrm.ttf",
+    "cmunrm.ttf",
+  ]
+  const socials:string[] =[
+    "instagram.com/bergenfont",
+    "instagram.com/bergenfont",
+    "instagram.com/bergenfont",
   ]
   return (
     <>
       <div className="pageblock" spellCheck="false">
         <h1 className="title">Bergen Font</h1>
-        <PosterGrid images={images} fonts={fonts} columns={3}/>
+        <PosterGrid images={images} fonts={fonts} socials={socials} columns={3}/>
         {/* <h3>Nylige funn:</h3>
         <div className="homeList">
           {loadFontList("/app/data/homeFonts.json")}
